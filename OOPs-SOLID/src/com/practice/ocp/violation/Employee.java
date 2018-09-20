a47c84a1914502012f42304799cb8b43f35c5257
prabhudeepsb <prabhudeepsb@gmail.com>
2018-09-21 01:39:02 +0530

    OCP Violation

---
+++ OOPs-SOLID/src/com/practice/ocp/violation/Employee.java
@@ -0,0 +1,26 @@
+package com.practice.ocp.violation;
+
+public class Employee {
+	
+	private String name;
+	private double salary;
+	private double bonus;
+	private String employeeType;
+	
+	public Employee(String name, double salary, String employeeType) {
+		this.name = name;
+		this.salary = salary;
+		this.employeeType = employeeType;
+	}
+	
+	public void calculateBonus() {
+		if(this.employeeType.equals("Permanent"))
+			this.bonus = this.salary*0.10;
+		else
+			if(this.employeeType.equals("Contract"))
+				this.bonus = this.salary*0.05;
+		
+		//Note that here if we want to add any another type of employee
+		//then we have to modify this class
+	}
+}
